9bb6cf050af3ae283050e38859fcdc065dcd4609
unknown <dhloc.se@gmail.com>
2021-10-17 12:38:33 +0700

    create conflicts

--- src/main/App.java
+++ src/main/App.java
@@ -28,7 +28,7 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
 
         
-        
+        System.out.println("hello1");
         
         Parent root = FXMLLoader.load(getClass().getResource("/view_admin/UILogIn.fxml"));
 
