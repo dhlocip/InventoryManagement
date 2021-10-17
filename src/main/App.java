943c402113aafb4d5076eff171fa96629f43828e
dhlocip <dhlocip@gmail.com>
2021-10-17 12:43:05 +0700

    resole conflicts

--- src/main/App.java
+++ src/main/App.java
@@ -27,8 +27,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/view_admin/UI.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/view_admin/UIc.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view_sale_manager/.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
