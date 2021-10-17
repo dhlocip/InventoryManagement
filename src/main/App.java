6e6232b650a805e2f987be453cda4e66bbebbb35
dhlocip <dhlocip@gmail.com>
2021-10-17 12:17:40 +0700

    test

--- src/main/App.java
+++ src/main/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/view_admin/UILogIn.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view_admin/UI.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
