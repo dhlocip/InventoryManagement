1221ef9adb20dd447d7db081c2bb19e3588e0d93
lethithuytrang5855 <lethithuytrang5855@gmail.com>
2021-10-20 17:48:14 +0700

    Delete event

--- src/controller_sale_manager/UpdateEventController.java
+++ src/controller_sale_manager/UpdateEventController.java
@@ -4,6 +4,8 @@
  */
 package controller_sale_manager;
 
+import data.EventDetail;
+import data.Events;
 import data.VEvent;
 import data_modifier.VEventModifier;
 import java.net.URL;
@@ -17,6 +19,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -25,6 +28,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -41,8 +45,6 @@ public class UpdateEventController implements Initializable {
     @FXML
     private DatePicker txtStartDate;
     @FXML
-    private Label updateEvent;
-    @FXML
     private TableView<VEvent> updateEventTable;
     @FXML
     private TableColumn<VEvent, String> eventId;
@@ -56,8 +58,6 @@ public class UpdateEventController implements Initializable {
     private TableColumn<VEvent, String> userId;
     @FXML
     private TableColumn<VEvent, String> startDate;  
-    @FXML
-    private Button updatebtn;
 
     @FXML
     private TableColumn<VEvent, String> endDate;
@@ -93,23 +93,89 @@ public class UpdateEventController implements Initializable {
         updateEventTable.setItems(oList);
     }
 
+    
     @FXML
-    private void getUpdate(ActionEvent event) {        
+    private void updateEventClicked(MouseEvent event) {        
+        System.out.println(txtStartDate.getValue());
         
     }
-//    private void setCellValueFromTableToTextField(){
-//        updateEventTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent t) {
-//            
-//             updateEventTable.getItems().get(updateEventTable.getSelectionModel().getSelectedIndex());
-//             
-//            }
-//        }
-//        
-//        );
-//    
-//    }
-//    
+    
+     @FXML
+    private void getDelete(ActionEvent event) {     
+              
+    if(updateEventTable.getSelectionModel().getSelectedItem() != null){
+        
+    
+    };
+    
+    
+        
+    
+    }
+    
+    
+    
     
 }
+//
+// @FXML
+//    private void updateProductClicked(MouseEvent event) throws SQLException {
+//
+//        if (lProductTypeId != null && isPriceRight() && isUnitRight() && isProNameRight()) {
+//            Products items = productDetailTableView.getSelectionModel().getSelectedItem();
+//            lPrice = Double.parseDouble(priceTextField.getText());
+//            lUnit = unitTextField.getText();
+//            lProductName = productNameTextField.getText();
+//            lProductTypeId = productTypeIdComboBox.getValue();
+//
+//            if (items != null) {
+//                items.setProductTypeId(lProductTypeId);
+//                items.setProductName(lProductName);
+//                items.setUnit(lUnit);
+//                items.setPrice(lPrice);
+//
+//                if (new ProductsModifier().updateProducts(items)) {
+//                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//                    alert.setTitle("Notification");
+//                    alert.setHeaderText("Success");
+//                    alert.setContentText("update product is successfully.");
+//                    alert.showAndWait();
+//
+////                reload table products 
+//                    getProductsInfo();
+//                }
+//            }
+//
+//        } else {
+//            Alert alert = new Alert(Alert.AlertType.ERROR);
+//            alert.setTitle("Notification");
+//            alert.setHeaderText("Error");
+//            alert.setContentText("Please click to item.");
+//            alert.showAndWait();
+//
+//            if (!isPriceRight()) {
+//                hideErrorOfPrice(true);
+//                errorOfPrice.setText("Price is invalid.");
+//            }
+//
+//            if (!isUnitRight()) {
+//                hideErrorOfUnit(true);
+//                errorOfUnit.setText("Unit is invalid.");
+//            }
+//
+//            if (!isProNameRight()) {
+//                hideErrorOfProName(true);
+//                errorOfProductName.setText("ProductName is invalid.");
+//            }
+//
+//            if (lProductTypeId == null) {
+//                hideErrorOfProTypeId(true);
+//                errorOfProductTypeId.setText("ProductTypeId can't empty.");
+//            }
+//        }
+//    }
+//
+//    @FXML
+//    private void searchReleased(KeyEvent event) throws SQLException {
+//        getProductsInfoWhenSearch(searchTextField.getText());
+//    }
--- src/data_modifier/VEventModifier.java
+++ src/data_modifier/VEventModifier.java
@@ -5,7 +5,11 @@
  */
 package data_modifier;
 
+import data.BillDetail;
+import data.EventDetail;
+import data.Events;
 import data.VEvent;
+import static data_modifier.JDBCConnect.connect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -19,32 +23,95 @@ import javafx.collections.ObservableList;
  * @author ADMIN
  */
 public class VEventModifier extends JDBCConnect {
-    
-    public ObservableList<VEvent> getEventInfo() throws SQLException{
-    ObservableList<VEvent> oList = FXCollections.observableArrayList();
-    String sql = "Select * from VEvent"; //viewsql
-    PreparedStatement preStatement= connect().prepareStatement(sql);
-    preStatement.execute();
-    ResultSet result = preStatement.getResultSet();
+
+    public ObservableList<VEvent> getEventInfo() throws SQLException {
+        ObservableList<VEvent> oList = FXCollections.observableArrayList();
+        String sql = "Select * from VEvent"; //viewsql
+        PreparedStatement preStatement = connect().prepareStatement(sql);
+        preStatement.execute();
+        ResultSet result = preStatement.getResultSet();
         while (result.next()) {
-            oList.add(new VEvent(result.getString("eventId"),result.getString("userId"),result.getString("eventName"), 
-                    result.getString("startDate"), result.getString("endDate")
-                    , result.getString("productId"), result.getString("discount"), result.getString("mfgDate"), 
+            oList.add(new VEvent(result.getString("eventId"), result.getString("userId"), result.getString("eventName"),
+                    result.getString("startDate"), result.getString("endDate"),
+                    result.getString("productId"), result.getString("discount"), result.getString("mfgDate"),
                     result.getString("expDate"))); //tencotsql
         }
         return oList;
     }
-    
-    
-//    public static void main(String[] args) throws SQLException {
-//        ObservableList<VEvent> oList = new VEventModifier().getInfo();
-//        for (VEvent vEvent : oList) {
-//            System.out.println(vEvent.getEventName());
-//        }
-//        ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    
-//        
-        
-//    }
-    
+
+    public ObservableList<Events> getDEventsInfo() throws SQLException {
+        ObservableList<Events> oList = FXCollections.observableArrayList();
+        String sql = "Select * from Events"; //viewsql
+        PreparedStatement preStatement = connect().prepareStatement(sql);
+        preStatement.execute();
+        ResultSet result = preStatement.getResultSet();
+        while (result.next()) {
+            oList.add(new Events(result.getString("eventId"), result.getString("userId"), result.getString("eventName"),
+                    result.getString("startDate"), result.getString("endDate"))); //tencotsql
+        }
+        return oList;
+    }
+
+    public ObservableList<EventDetail> getEventDetailInfo() throws SQLException {
+        ObservableList<EventDetail> oList = FXCollections.observableArrayList();
+        String sql = "Select * from EventDetail"; //viewsql
+        PreparedStatement preStatement = connect().prepareStatement(sql);
+        preStatement.execute();
+        ResultSet result = preStatement.getResultSet();
+        while (result.next()) {
+            oList.add(new EventDetail(result.getString("eventId"), result.getString("productId"),
+                    result.getString("discount"), result.getString("mfdDate"), result.getString("expDate"))); //tencotsql
+        }
+        return oList;
+    }
+
+    public boolean deleteEventDetailByEventId(String eventId) throws SQLException {
+        String sql = "delete from eventDetail "
+                + "where eventId = ?";
+        PreparedStatement preStatement = connect().prepareStatement(sql);
+        preStatement.setString(1, eventId);
+        preStatement.execute();
+        return true;
+    }
+
+    public boolean deleteEventByEventId(String eventId) throws SQLException {
+        String sql = "delete from events "
+                + "where eventId = ?";
+        PreparedStatement preStatement = connect().prepareStatement(sql);
+        preStatement.setString(1, eventId);
+        preStatement.execute();
+        return true;
+    }
+
+    public ObservableList<String> getListEventId() throws SQLException {
+        ObservableList<String> oList = FXCollections.observableArrayList();
+        String sql = "select * from Events";
+        PreparedStatement preStatement = connect().prepareStatement(sql);
+        preStatement.execute();
+        ResultSet result = preStatement.getResultSet();
+        while (result.next()) {
+            oList.add(result.getString("eventId"));
+        }
+        return oList;
+    }
+
+    public ObservableList<VEvent> getInfoByEventId(String eventId) throws SQLException {
+        ObservableList<VEvent> oList = FXCollections.observableArrayList();
+        String sql;
+        PreparedStatement preStatement;
+
+        sql = "select * from VEvent "
+                + " where eventId like '%" + eventId + "%'";
+        preStatement = connect().prepareStatement(sql);
+        preStatement.execute();
+        ResultSet result = preStatement.getResultSet();
+        while (result.next()) {
+            oList.add(new VEvent(result.getString("eventId"), result.getString("userId"), result.getString("eventName"),
+                    result.getString("startDate"), result.getString("endDate"),
+                    result.getString("productId"), result.getString("discount"), result.getString("mfgDate"),
+                    result.getString("expDate"))); //tencotsql
+        }
+        return oList;
+    }
+
 }
